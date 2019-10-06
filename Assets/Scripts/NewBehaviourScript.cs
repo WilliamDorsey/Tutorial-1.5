d26f042e0b0bdbd4f897aa703ac4eddc7624d9d4
WilliamDorsey <55061558+WilliamDorsey@users.noreply.github.com>
2019-10-06 14:29:59 -0400

    Added files

---
+++ Assets/Scripts/NewBehaviourScript.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+
+    public AudioSource musicSource;
+
+    public AudioClip musicClipOne;
+
+    public AudioClip musicClipTwo;
+
+    Animator anim;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            musicSource.clip = musicClipOne;
+            musicSource.Play();
+            anim.SetInteger("state", 1);
+        }
+
+        if (Input.GetKeyUp(KeyCode.W))
+        {
+            musicSource.Stop();
+            anim.SetInteger("state", 0);
+        }
+
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            musicSource.clip = musicClipTwo;
+            musicSource.Play();
+            anim.SetInteger("state", 2);
+        }
+
+        if (Input.GetKeyUp(KeyCode.R))
+        {
+            musicSource.Stop();
+            anim.SetInteger("state", 0);
+        }
+
+
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            musicSource.loop = true;
+        }
+
+        if (Input.GetKeyUp(KeyCode.L))
+        {
+            musicSource.loop = false;
+        }
+
+
+        if (Input.GetKey("escape"))
+        {
+            Application.Quit();
+        }
+    }
+}
